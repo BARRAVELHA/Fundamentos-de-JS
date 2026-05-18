f4ffe2a429782e9da3e1f4c92dccf714bd991cb6
BARRAVELHA <matheusnplatt@gmail.com>
2026-05-18 08:57:18 -0300

    Delete num.js

--- num.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-
-let resposta = 0
-for(let i=1; i<=100;i++){
-    resposta=resposta+i;
-
-    console.log("resposta:"+resposta)
-}
-    console.log("resposta final:"+resposta)
