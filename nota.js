6d7950dbb308d3ede826353c63e1c4ec0424a087
BARRAVELHA <matheusnplatt@gmail.com>
2026-05-18 08:56:26 -0300

    Delete nota.js

--- nota.js
+++
@@ -1,7 +0,0 @@
-const prompt=require("prompt-sync")();
-let nota = Number(prompt("nota final de um aluno:"))
-if(nota>=7){
-    console.log("nota:"+nota+" aprovação")
-}else{
-    console.log("nota:"+nota+"reprovado")
-}
