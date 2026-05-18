c68e3ec74e59dd08456404027422f3d8e328d783
BARRAVELHA <matheusnplatt@gmail.com>
2026-05-18 08:57:08 -0300

    Delete numero.js

--- numero.js
+++
@@ -1,9 +0,0 @@
-const prompt=require("prompt-sync")();
-let numero = Number(prompt("digite seu numero:"));
-if(numero ==0){
-    console.log("nomero neutro:"+numero)
-}else if(numero >=1){
-    console.log("seu numero e positivo:"+numero)
-}else{
-    console.log("seu numero e negativo:"+numero)
-}
