c279320d8225addcf928d4d9231e9594eb3cfe9b
BARRAVELHA <matheusnplatt@gmail.com>
2026-05-18 09:14:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 
-const prompt = require("prompt-sync")();
-
-let idade = Number(prompt("digite sua idade: "));
-
-let nome = prompt("digite seu nome: ");
-
-if(idade>=18){
-    console.log(nome+"voce e maior de idade.");
-}else{
-    console.log(nome+"voce e menor de idade.");
-}
