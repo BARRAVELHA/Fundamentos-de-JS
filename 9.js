ab51a03b6a3d27c75649584e4d3f12d44cf303b5
BARRAVELHA <matheusnplatt@gmail.com>
2026-05-18 13:11:24 -0300

    Update 9.js

--- 9.js
+++ 9.js
@@ -1,1 +1,9 @@
+const prompt = require("prompt-sync")();
+let numeros=[]
 
+for(let i=1; i<= 5;i++){
+    let numero=prompt("digite seu numero:");
+    numeros.push(numero)
+}
+let maior = Math.max(...numeros)
+ console.log("o numero de menor para maior:",maior)
