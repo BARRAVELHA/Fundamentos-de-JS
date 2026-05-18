933c9088ff92ab384052ca2ab7f3da50ce57bce9
BARRAVELHA <matheusnplatt@gmail.com>
2026-05-18 08:56:14 -0300

    Delete mate.js

--- mate.js
+++
@@ -1,5 +0,0 @@
-const prompt=require("prompt-sync")();
-let numero = Number(prompt("digite um numero:"))
-for(let i=1 ; i<=10;i++){
-    let mate = (i * numero);
-    console.log("numero:"+numero+"X"+i+"=",mate);}
